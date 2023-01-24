28e45c4ad4bb8d2e9840ff3c21653a3b0c11bf9e
emjay <adisamujib11@gmail.com>
2023-01-24 13:22:03 -0800

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+*selection_sort - swaps in order by comparing from marker
+*@array: the array
+*@size: array length size
+*Return: void
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, walker = 0, to_swap = 0;
+	int to_comp = 0;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		to_comp = array[i];
+		to_swap = i;
+		for (walker = i; walker < size; walker++)
+		{
+			if (array[walker] < to_comp)
+			{
+				to_swap = walker;
+				to_comp = array[walker];
+			}
+		}
+		if (to_swap != i)
+		{
+			array[i] += array[to_swap];
+			array[to_swap] = array[i] - array[to_swap];
+			array[i] -= array[to_swap];
+			print_array(array, size);
+		}
+	}
+}
